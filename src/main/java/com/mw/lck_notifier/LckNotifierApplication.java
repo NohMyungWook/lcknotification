ca015cae56ecf5f50525a3c414a234eb095cd6e7
nohmw <nohmw@adenasoft.com>
2025-12-30 16:23:30 +0900

    init spring porject

---
+++ src/main/java/com/mw/lck_notifier/LckNotifierApplication.java
@@ -0,0 +1,13 @@
+package com.mw.lck_notifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LckNotifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LckNotifierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mw/lck_notifier/LckNotifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mw.lck_notifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LckNotifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
