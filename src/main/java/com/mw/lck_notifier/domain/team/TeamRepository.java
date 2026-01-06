b0992aff8df9802614ffedbcb6be68661cc45ada
nohmw <nohmw@adenasoft.com>
2026-01-06 18:32:50 +0900

    add team repository

---
+++ src/main/java/com/mw/lck_notifier/domain/team/TeamRepository.java
@@ -0,0 +1,13 @@
+package com.mw.lck_notifier.domain.team;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TeamRepository extends JpaRepository<Team, Long> {
+
+    Optional<Team> findByName(String name);
+
+    List<Team> findAllByActiveTrue();
+}
